aa2e970b18cfac4031e92a0a54cf3a5db9150fdf
Tomás Pinto <tomasp834@gmail.com>
2019-08-09 02:43:40 -0300

    obtain title/description, then save to csv

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import requests
 from urllib.request import urlopen
 import time
 from bs4 import BeautifulSoup
-print(urlopen)
-url = "https://tusecreto.io/secret/2342"
+import csv
+
+
+url = 'https://tusecreto.io/secret/12133584'
 response = requests.get(url)
-print(response)
+soup = BeautifulSoup(response.text, 'html.parser')
+title = soup.find('meta', property='og:title')['content'][15:]
+description = soup.find('meta', property='og:description')['content']
+
+secrets = [(title, description)]
+
+with open('ts_data.csv', 'w') as file_handler:
+	csv_out = csv.writer(file_handler)
+	csv_out.writerow(['titulo', 'secreto'])
+	for row in secrets:
+		csv_out.writerow(row)
