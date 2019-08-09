1f48e03f5b1eb2cc69f4ffa7536711fe249c1c25
Tomás Pinto <tomasp834@gmail.com>
2019-08-09 03:28:17 -0300

    write gender/age to csv

--- main.py
+++ main.py
@@ -10,7 +10,27 @@ def write_secret(secret):
     csv_out = csv.writer(file_handler)
     csv_out.writerow(secret)
 
+MALE = "hombre"
+MALE_SYMBOL = "H"
+FEMALE = "mujer"
+FEMALE_SYMBOL = "M"
+
+def get_gender(secret_title):
+  if MALE in secret_title:
+    return MALE_SYMBOL
+  elif FEMALE in secret_title:
+    return FEMALE_SYMBOL
+  else:
+    return "undefined"
+
+AGE_START = -7
+AGE_END = -5
+
+def get_age(secret_title):
+  return secret_title[AGE_START:AGE_END]
+
 TITLE_START = 15
+UNDEFINED = "undefined"
 
 secret_id = 0
 
@@ -22,5 +42,11 @@ for x in range(0, 10):
   soup = BeautifulSoup(response.text, 'html.parser')
   title = soup.find('meta', property='og:title')['content'][TITLE_START:]
   description = soup.find('meta', property='og:description')['content']
-  write_secret((title, description))
+  if UNDEFINED in title and UNDEFINED in description:
+    secret_id += 1
+    time.sleep(1)
+    continue
+  gender = get_gender(title)
+  age = get_age(title)
+  write_secret((gender, age, description))
   secret_id += 1
