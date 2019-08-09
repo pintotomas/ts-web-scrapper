fb7f73fda0a0a49c878aa19b2e891a4039c48a6e
Tomás Pinto <tomasp834@gmail.com>
2019-08-09 03:08:12 -0300

    write right after getting information

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ import time
 from bs4 import BeautifulSoup
 import csv
 
+def write_secret(secret):
+  # receives (title, description) and writes out to ts_data.csv
+  with open('ts_data.csv', 'a', newline='', encoding='utf-8-sig') as file_handler:
+    csv_out = csv.writer(file_handler)
+    csv_out.writerow(secret)
 
-url = 'https://tusecreto.io/secret/12133584'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-title = soup.find('meta', property='og:title')['content'][15:]
-description = soup.find('meta', property='og:description')['content']
+TITLE_START = 15
 
-secrets = [(title, description)]
+secret_id = 0
 
-with open('ts_data.csv', 'w') as file_handler:
-	csv_out = csv.writer(file_handler)
-	csv_out.writerow(['titulo', 'secreto'])
-	for row in secrets:
-		csv_out.writerow(row)
+secrets = []
+
+for x in range(0, 10):
+  url = 'https://tusecreto.io/secret/'+str(secret_id)
+  response = requests.get(url)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  title = soup.find('meta', property='og:title')['content'][TITLE_START:]
+  description = soup.find('meta', property='og:description')['content']
+  write_secret((title, description))
+  secret_id += 1
